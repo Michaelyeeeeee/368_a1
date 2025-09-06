0722401b3da4b32d5f35a16573cfeb680c0c0e77
Michael Wu <michaeldmwu@gmail.com>
2025-09-06 18:50:13 -0400

    e

--- chars.h
+++ chars.h
@@ -6,3 +6,5 @@
 int getFileLength(char * filename);
 int getChars(char * filename, int * firstChar, int * secondChar);
 void printList(int * list, int length);
+
+int reachable(int src, int target, int adj[26][26], int visited[26]);
